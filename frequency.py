b2fe99a40885b5372ca7f933434ade18b5bb7683
Shriraj Ghadigaonkar <ghadigaonkarshriraj@gmail.com>
2026-09-13 01:50:37 +0530

    Add character Frequency program

---
+++ frequency.py
@@ -0,0 +1,10 @@
+string = input("Enter a string:")
+
+frequency = {}
+for char in string:
+    if char in frequency:
+        frequency[char] += 1
+    else:
+        frequency[char] = 1
+
+print("Character Frequency:", frequency)
