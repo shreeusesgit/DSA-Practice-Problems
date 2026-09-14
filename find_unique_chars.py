00fdd239cb6578970b3cde421f9c221b35dda03b
Shriraj Ghadigaonkar <ghadigaonkarshriraj@gmail.com>
2026-09-14 12:32:56 +0530

    Add function to find and print unique characters

---
+++ find_unique_chars.py
@@ -0,0 +1,10 @@
+string = "swiss"
+
+frequency = {}
+
+for char in string:
+    frequency[char] = frequency.get(char, 0) + 1
+
+for char in string:
+    if frequency[char] == 1:
+        print(char)
